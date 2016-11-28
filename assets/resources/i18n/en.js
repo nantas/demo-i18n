2f46234af73abbd601c46ff4b9b2a4528c6647f4
Nan Wang <nantas@gmail.com>
2016-11-28 14:39:04 +0800

    init commit for i18n demo

---
+++ assets/resources/i18n/en.js
@@ -0,0 +1,12 @@
+if (!window.i18n) window.i18n = {};
+window.i18n.en={
+// write your key value pairs here
+    "label": {
+        "hello": "Hello!",
+        "bye": "Goodbye!"
+    },
+    "sprite": {
+        "btn_combat": "resources/localized-image/en/start",
+        "btn_home": "resources/localized-image/en/home"
+    }
+};
---
+++ assets/resources/i18n/zh.js
@@ -0,0 +1,12 @@
+if (!window.i18n) window.i18n = {};
+window.i18n.zh={
+// write your key value pairs here
+    "label": {
+        "hello": "你好！",
+        "bye": "再见！"
+    },
+    "sprite": {
+        "btn_combat": "resources/localized-image/zh/start",
+        "btn_home": "resources/localized-image/zh/home"
+    }
+};
