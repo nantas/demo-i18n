7e0da29777570a0c517dd7846d445f14b8a8d588
Nan Wang <nantas@gmail.com>
2016-12-02 18:53:22 +0800

    update

--- assets/resources/i18n/en.js
+++ assets/resources/i18n/en.js
@@ -1,12 +1,8 @@
-if (!window.i18n) window.i18n = {};
-window.i18n.en={
+if (!window.i18n) window.i18n = {languages:{}, curLang:""};
+window.i18n.languages.en={
 // write your key value pairs here
     "label": {
         "hello": "Hello!",
         "bye": "Goodbye!"
-    },
-    "sprite": {
-        "btn_combat": "resources/localized-image/en/start",
-        "btn_home": "resources/localized-image/en/home"
     }
 };
--- assets/resources/i18n/zh.js
+++ assets/resources/i18n/zh.js
@@ -1,12 +1,8 @@
-if (!window.i18n) window.i18n = {};
-window.i18n.zh={
+if (!window.i18n) window.i18n = {languages:{}, curLang:""};
+window.i18n.languages.zh={
 // write your key value pairs here
     "label": {
         "hello": "你好！",
         "bye": "再见！"
-    },
-    "sprite": {
-        "btn_combat": "resources/localized-image/zh/start",
-        "btn_home": "resources/localized-image/zh/home"
     }
 };
