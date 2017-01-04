1907aea1afcb2a4be975f5cea0762569ee27b63c
Nan Wang <nantas@gmail.com>
2017-01-04 11:40:01 +0800

    fixed i18n data format

--- assets/resources/i18n/en.js
+++ assets/resources/i18n/en.js
@@ -1,8 +1,9 @@
-if (!window.i18n) window.i18n = {languages:{}, curLang:""};
+if (!window.i18n) window.i18n = {};
+if (!window.i18n.languages) window.i18n.languages = {};
 window.i18n.languages.en={
 // write your key value pairs here
-    "label": {
-        "hello": "Hello!",
-        "bye": "Goodbye!"
+    label: {
+        hello: 'Hello!',
+        bye: 'Goodbye!'
     }
 };
--- assets/resources/i18n/zh.js
+++ assets/resources/i18n/zh.js
@@ -1,8 +1,9 @@
-if (!window.i18n) window.i18n = {languages:{}, curLang:""};
+if (!window.i18n) window.i18n = {};
+if (!window.i18n.languages) window.i18n.languages = {};
 window.i18n.languages.zh={
 // write your key value pairs here
-    "label": {
-        "hello": "你好！",
-        "bye": "再见！"
+    label: {
+        hello: '你好！',
+        bye: '再见！'
     }
 };
